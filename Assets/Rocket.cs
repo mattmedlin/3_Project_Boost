243871ba1bf7a760de89a6f74183e77d19d1a7da
mattmedlin <50458786+mattmedlin@users.noreply.github.com>
2020-05-12 16:45:59 -0500

    adding tags and serialized fields for thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 10f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -19,16 +22,33 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
-    
-        
+        Thrust();
+        Rotate();
+
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                print("OK"); // todo remove
+                break;
+            case "Fuel":
+                print("Fuel"); // todo remove
+                break;
+            default:
+                print("Kill"); // todo remove
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -38,15 +58,25 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
-            
-        
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = Time.deltaTime * rcsThrust;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
+
+    
 }
